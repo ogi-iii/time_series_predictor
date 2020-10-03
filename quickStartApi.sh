33c4110b4cd4a88ffa02784117534f28a9c98b7c
*TlSunGQf66 <kazuki@hacarus.com>
2020-10-03 19:01:39 +0900

    script modified

--- quickStartApi.sh
+++ quickStartApi.sh
@@ -2,6 +2,9 @@
 cd api/
 
 # TSPUploadLambdaApi
+echo
+echo "[1/3] Deploying tsp-upload-api..."
+echo
 aws s3 mb s3://tsp-upload-template-ogi
 cd tsp-upload/
 
@@ -15,8 +18,10 @@ aws cloudformation deploy \
     --stack-name tsp-upload-api \
     --capabilities CAPABILITY_IAM
 
-
 # TSPHistoryLambdaApi
+echo
+echo "[2/3] Deploying tsp-hist-api..."
+echo
 aws s3 mb s3://tsp-hist-template-ogi
 cd ../tsp-history/
 
@@ -30,15 +35,20 @@ aws cloudformation deploy \
     --stack-name tsp-hist-api \
     --capabilities CAPABILITY_IAM
 
-
-# Write endpoints in Dockerfile
+# Edit Dockerfile
+echo
+echo "[3/3] Write endpoints in Dockerfile..."
+echo
 cd ../../
+sed -e '$d' Dockerfile > temp.txt
+rm Dockerfile
+mv temp.txt Dockerfile
 sudo yum -y install jq
-
 uploadapi=$(aws cloudformation describe-stacks --stack-name tsp-upload-api | jq '.Stacks'[0]'.Outputs'[0]'.OutputValue')
+echo tsp-upload-api ${uploadapi}
+echo
 histapi=$(aws cloudformation describe-stacks --stack-name tsp-hist-api | jq '.Stacks'[0]'.Outputs'[0]'.OutputValue')
-
-echo "[NEXT STEP] add this command to the bottom line in Dockerfile"
-echo ""
-echo CMD [${uploadapi}, ${histapi}]
-echo ""
+echo tsp-hist-api ${histapi}
+echo
+echo CMD [${uploadapi}, ${histapi}] >> Dockerfile
+echo "[Success] All shell tasks are completed."
