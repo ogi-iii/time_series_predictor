c39a1e5b638b58e64784221acc47b4f4626e1bc4
*TlSunGQf66 <kazuki@hacarus.com>
2020-10-04 16:48:53 +0900

    templates fixed

--- quickStartEcsApp.sh
+++ quickStartEcsApp.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
+#! /bin/bash -eu
 cd aws-cfns/
 
 # VPC
 echo
 echo VPC
-echo
 aws cloudformation deploy \
     --template-file vpc.yaml \
     --stack-name tsp-vpc
@@ -12,7 +11,6 @@ aws cloudformation deploy \
 # SecurityGroup
 echo
 echo SecurityGroup
-echo
 aws cloudformation deploy \
     --template-file securitygroup.yaml \
     --stack-name tsp-securitygroup
@@ -20,33 +18,42 @@ aws cloudformation deploy \
 # CodeCommit
 echo
 echo CodeCommit
-echo
 aws cloudformation deploy \
     --template-file codecommit.yaml \
     --stack-name tsp-codecommit
 
+# git push to CodeCommit
+cd ../
+git clone https://git-codecommit.ap-northeast-1.amazonaws.com/v1/repos/tsp-codecommit-py-app
+cd appContainer/
+cp -r * ../tsp-codecommit-py-app/
+cd ../tsp-codecommit-py-app/
+git add .
+git commit -m "init commit"
+git push
+cd ../aws-cfns/
+
 # ECR
 echo
 echo ECR
-echo
 aws cloudformation deploy \
     --template-file ecr.yaml \
     --stack-name tsp-ecr
 aws ecr put-image-scanning-configuration --repository-name tsp-ecr-py-app --image-scanning-configuration scanOnPush=true
 
 # Push App to ECR
-cd ../appContainer/
+cd ../tsp-codecommit-py-app/
 aws --version
 $(aws ecr get-login --region ap-northeast-1 --no-include-email)
-REPOSITORY_URI=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_NAME}
-docker build -t ${REPOSITORY_URI}:latest .
-docker push ${REPOSITORY_URI}:latest
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+IMAGE_URI=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/tsp-ecr-py-app
+docker build -t ${IMAGE_URI}:latest .
+docker push ${IMAGE_URI}:latest
 cd ../aws-cfns/
 
 # ECS-Cluster
 echo
 echo ECS Cluster
-echo
 aws cloudformation deploy \
     --template-file ecs-cluster.yaml \
     --stack-name tsp-ecs-cluster
@@ -54,9 +61,6 @@ aws cloudformation deploy \
 # App-ALB-Fargate
 echo
 echo App ALB Fargate
-echo
-ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-IMAGE_URI=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/tsp-ecr-py-app
 aws cloudformation deploy \
     --template-file app-alb-fargate.yaml \
     --stack-name tsp-app-alb-fargate \
@@ -64,17 +68,9 @@ aws cloudformation deploy \
     --parameter-overrides \
     ImageUri=${IMAGE_URI}
 
-# git push to CodeCommit
-cd ../appContainer/
-git add .
-git commit -m "init commit"
-git push
-cd ../aws-cfns/
-
 # CodePipeLine
 echo
 echo CodePipeLine
-echo
 aws cloudformation deploy \
     --template-file app-codepipeline.yaml \
     --stack-name tsp-app-codepipeline
