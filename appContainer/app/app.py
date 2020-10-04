32eca265416cbb3fe952f82bd6f17d87016e224b
*TlSunGQf66 <kazuki@hacarus.com>
2020-10-04 19:26:17 +0900

    port modified

--- appContainer/app/app.py
+++ appContainer/app/app.py
@@ -126,4 +126,4 @@ def history():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=80)
--- quickRunLocalApp.sh
+++ quickRunLocalApp.sh
@@ -2,5 +2,5 @@
 cd appContainer/
 # Docker
 docker image build -t tsp/app:latest .
-docker container run -p 8000:8000 --rm tsp/app:latest
+docker container run -p 80:80 --rm tsp/app:latest
 cd ../
